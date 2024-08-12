1cc57c7038f1a0167cef5e97bac9aec127720535
ihorLysak <lysak.ihor4221@gmail.com>
2024-08-12 23:34:36 +0300

    moved data storage to home directory

--- warp.sh
+++ warp.sh
@@ -2,12 +2,12 @@
 
 add_gate() {
     local gate_name="$1"
-    echo "$gate_name: $(pwd)" >> /tmp/gates.txt
+    echo "$gate_name: $(pwd)" >> ~/.warp/gates.txt
 }
 
 get_gate_path() {
     local gate_name="$1"
-    local file_path="/tmp/gates.txt"
+    local file_path="~/.warp/gates.txt"
     
     if [ ! -f "$file_path" ]; then
         echo "The gate file does not exist."
@@ -26,7 +26,7 @@ get_gate_path() {
 
 remove_gate() {
     local gate_name="$1"
-    local file_path="/tmp/gates.txt"
+    local file_path="~/.warp/gates.txt"
 
     if [ ! -f "$file_path" ]; then
         echo "The gate file does not exist."
